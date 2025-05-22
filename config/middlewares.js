4c2f472cccfe1cc1fcfe97f3e842b09daeeabb81
sharjeel262 <sharjeelali262@gmail.com>
2025-05-22 19:32:22 +0500

    Initial commit

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,16 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // add your frontend URL here
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
